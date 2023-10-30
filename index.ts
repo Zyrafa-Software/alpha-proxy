171c0757507634a74116abd1b5bce501c64c83f8
Alex89198900 <alexchorn1989@ukr.net>
2023-10-30 18:06:33 +0200

    feat: mobiteam

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { updateTelemetry } from "./mobiteam/updateTelemetry";
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 import cors from "cors";
+import { Database } from "./mobiteam/types/database";
 const path = require("path");
 const app = express();
 app.use(cors());
@@ -12,15 +14,22 @@ app.use(cookieParser());
 const request = require("request");
 const urlSesion =
   "https://gps.mobiteam.com.ua/api/integration/v1/connect?login=Erp&password=100930&lang=en-us&timezone=3";
-const urlTrucks =
+const urlTruck =
   "https://gps.mobiteam.com.ua/api/integration/v1/fullobjinfo?oid=8748";
+const trucksChange =
+  "https://gps.mobiteam.com.ua//api/integration/v1/objectsupdateinfo";
+interface TypeTruck {
+  oid: number;
+  dt: string;
+  lat: number;
+  lon: number;
+  dir: number;
+  move: number;
+  st: number;
+}
 
+let truckFullInfo: any[] = [];
 
-let trucksRes='';
-const trackInfo = {
-  all: [],
-  update: [],
-};
 const coockieVar = "set-cookie";
 
 const startRequests = async () => {
@@ -35,9 +44,9 @@ const startRequests = async () => {
     },
     function (error: any, response: any, body: any) {
       if (!error && response.statusCode == 200) {
-        startRequestTruck(response.headers[coockieVar]);
+        startRequestTrucksChange(response.headers[coockieVar]);
         setInterval(
-          () => startRequestTruck(response.headers[coockieVar]),
+          () => startRequestTrucksChange(response.headers[coockieVar]),
           180000
         );
       } else {
@@ -46,14 +55,40 @@ const startRequests = async () => {
     }
   );
 };
-const startRequestTruck = async (cookie: any) => {
+const startRequestTrucksChange = async (cookie: any) => {
   await request(
     {
       method: "GET",
       headers: {
         Cookie: `${cookie[0]}; ${cookie[1]}`,
       },
-      url: urlTrucks,
+      url: trucksChange,
+      qs: {
+        param: "edit",
+        value: 100,
+      },
+    },
+    async function (error: any, response: any, body: any) {
+      if (!error && response.statusCode == 200) {
+        const arrChange = JSON.parse(body).objs;
+        truckFullInfo.length = 0;
+        await Promise.all(
+          arrChange.map((element: TypeTruck) => {
+            return startRequestTrucksInfo(cookie, element.oid);
+          })
+        );
+      }
+    }
+  );
+};
+const startRequestTrucksInfo = async (cookie: any, oid: any) => {
+  const res = await request(
+    {
+      method: "GET",
+      headers: {
+        Cookie: `${cookie[0]}; ${cookie[1]}`,
+      },
+      url: `https://gps.mobiteam.com.ua/api/integration/v1/fullobjinfo?oid=${oid}`,
       qs: {
         param: "edit",
         value: 100,
@@ -61,16 +96,18 @@ const startRequestTruck = async (cookie: any) => {
     },
     function (error: any, response: any, body: any) {
       if (!error && response.statusCode == 200) {
-        console.log(body)
-        trucksRes=body
+        //truckFullInfo.push(JSON.parse(body))
+        updateTelemetry(JSON.parse(body), new Date());
+        console.log('time')
       }
     }
   );
-  console.log("jjj");
+
+  return res;
 };
 
 app.get("/", async (req, res, next) => {
-  res.status(200).json(trucksRes);
+  res.status(200).json(truckFullInfo);
 });
 
 const start = async () => {
